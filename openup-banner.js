df92df4c2979aa32c70756fc3a907b0e1adcd092
Guus Hoekman <mail@guushoekman.com>
2017-05-25 13:15:49 +0200

    add logo, tweak banner

---
+++ openup-banner.js
@@ -0,0 +1,5 @@
+var openupBanner = '<div id="openup-banner" style="height: 50px; line-height: 50px; background-color: #ddd; font-size: 16px; color: #6d6e70; font-family: sans-serif; text-align: center;"> This is a project by <a href="https://openup.org.za"><img style="height: 50px;" src="https://openup.org.za/img/logo.svg"></a></div>'
+
+document.body.insertAdjacentHTML('beforeend', openupBanner);
+
+
